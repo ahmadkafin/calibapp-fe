8f03f87ea2ef7871b8551c2ee89b3d9d391f4a21
Ahmad Kafin <kafinahmad@gmail.com>
2025-10-22 09:08:18 +0700

    change viewport

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,84 +4,87 @@
       <div class="row">
         <img src="@@/assets/images/logo.png" alt="" class="rounded mx-auto d-block custom-image" />
       </div>
-      <div class="row">
-        <Form class="form" @submit="submitLogin" :validation-schema="schema">
-          <!-- phones -->
-          <div class="d-flex justify-content-center">
-            <div class="col-6">
-              <Field name="phone" v-slot="{ field, meta, errors }">
-                <label for="phone">
-                  Phone
-                  <small v-if="meta.touched && errors[0]" class="text-danger">
-                    &nbsp;({{ errors[0] }})
-                  </small>
-                </label>
-                <div class="input-group mb-3">
-                  <span class="input-group-text" id="phone-addon">
-                    <font-awesome-icon :icon="['fas', 'phone']" />
-                  </span>
-                  <input
-                    v-bind="field"
-                    type="text"
-                    class="form-control"
-                    placeholder="Phone (eg: 081xxxxxxx)"
-                    :class="{
-                      'is-invalid': meta.touched && errors[0],
-                      'is-valid': meta.touched && meta.valid,
-                    }"
-                  />
-                </div>
-              </Field>
-            </div>
-          </div>
-          <!-- end phone -->
 
-          <!-- password -->
-          <div class="d-flex justify-content-center">
-            <div class="col-6">
-              <Field name="password" v-slot="{ field, meta, errors }">
-                <label for="passworrd">
-                  Password
-                  <small v-if="meta.touched && errors[0]" class="text-danger">
-                    &nbsp;({{ errors[0] }})
-                  </small>
-                </label>
-                <div class="input-group mb-3">
-                  <span class="input-group-text" id="phone-addon">
-                    <font-awesome-icon :icon="['fas', 'key']" />
-                  </span>
-                  <input
-                    v-bind="field"
-                    :type="showMask ? 'text' : 'password'"
-                    å
-                    class="form-control"
-                    placeholder="Password"
-                    :class="{
-                      'is-invalid': meta.touched && errors[0],
-                      'is-valid': meta.touched && meta.valid,
-                    }"
-                  />
-                  <span class="input-group-text" id="mask-addon" @click="changeMask">
-                    <font-awesome-icon :icon="['fas', !showMask ? 'eye' : 'eye-slash']" />
-                  </span>
-                </div>
-              </Field>
-            </div>
-          </div>
-          <!-- end password -->
-
-          <!-- start button -->
-          <div class="d-flex justify-content-center">
-            <div class="col-6">
-              <div class="d-grid">
-                <button class="btn btn-success btn-sm" v-if="!isLoading">Submit</button>
-                <span class="loader mx-auto" v-else></span>
+      <Form class="form" @submit="submitLogin" :validation-schema="schema">
+        <!-- phones -->
+        <div class="row justify-content-center mb-3">
+          <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+            <Field name="phone" v-slot="{ field, meta, errors }">
+              <label for="phone">
+                Phone
+                <small v-if="meta.touched && errors[0]" class="text-danger">
+                  &nbsp;({{ errors[0] }})
+                </small>
+              </label>
+              <div class="input-group mb-1">
+                <span class="input-group-text" id="phone-addon">
+                  <font-awesome-icon :icon="['fas', 'phone']" />
+                </span>
+                <input
+                  id="phone"
+                  v-bind="field"
+                  type="text"
+                  class="form-control"
+                  placeholder="Phone (eg: 081xxxxxxx)"
+                  :class="{
+                    'is-invalid': meta.touched && errors[0],
+                    'is-valid': meta.touched && meta.valid,
+                  }"
+                />
+                <span class="input-group-text" id="phone-addon-2">
+                  <font-awesome-icon :icon="['fas', 'phone']" />
+                </span>
               </div>
+            </Field>
+          </div>
+        </div>
+        <!-- end phone -->
+
+        <!-- password -->
+        <div class="row justify-content-center mb-3">
+          <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+            <Field name="password" v-slot="{ field, meta, errors }">
+              <label for="password">
+                Password
+                <small v-if="meta.touched && errors[0]" class="text-danger">
+                  &nbsp;({{ errors[0] }})
+                </small>
+              </label>
+              <div class="input-group mb-1">
+                <span class="input-group-text" id="key-addon">
+                  <font-awesome-icon :icon="['fas', 'key']" />
+                </span>
+                <input
+                  id="password"
+                  v-bind="field"
+                  :type="showMask ? 'text' : 'password'"
+                  class="form-control"
+                  placeholder="Password"
+                  :class="{
+                    'is-invalid': meta.touched && errors[0],
+                    'is-valid': meta.touched && meta.valid,
+                  }"
+                />
+                <span class="input-group-text" id="mask-addon" role="button" @click="changeMask">
+                  <font-awesome-icon :icon="['fas', !showMask ? 'eye' : 'eye-slash']" />
+                </span>
+              </div>
+            </Field>
+          </div>
+        </div>
+        <!-- end password -->
+
+        <!-- start button -->
+        <div class="row justify-content-center">
+          <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+            <div class="d-grid">
+              <button class="btn btn-success btn-sm w-100" v-if="!isLoading">Submit</button>
+              <span class="loader d-block w-100 text-center" v-else></span>
             </div>
           </div>
-          <!-- end button -->
-        </Form>
-      </div>
+        </div>
+        <!-- end button -->
+      </Form>
     </div>
   </div>
 </template>
@@ -127,22 +130,34 @@ export default {
   },
 }
 </script>
-
 <style scoped>
+/* tetap pakai style image kamu */
 .page-wrap {
   min-height: 100dvh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 }
 
 .row .custom-image {
   width: clamp(140px, 40vw, 260px);
-  margin-top: 4dvh;
+  /* margin-top: 4dvh; */
   filter: brightness(0) invert(1);
+}
+
+@media (max-width: 767px) {
+  .row .custom-image {
+    width: clamp(100px, 150vw, 250px);
+    /* margin-top: 3dvh; */
+    filter: brightness(0) invert(1);
+  }
 }
 
 @media (min-width: 768px) {
   .row .custom-image {
     width: clamp(160px, 24vw, 300px);
-    margin-top: 5dvh;
+    /* margin-top: 5dvh; */
     filter: brightness(0) invert(1);
   }
 }
@@ -150,8 +165,21 @@ export default {
 @media (min-width: 1024px) {
   .row .custom-image {
     width: clamp(180px, 16vw, 320px);
-    margin-top: 6dvh;
+    /* margin-top: 6dvh; */
     filter: brightness(0) invert(1);
   }
+}
+
+/* opsional: samakan padding / gap antar form group */
+.form .input-group {
+  width: 100%;
+}
+
+/* jika loader punya ukuran khusus, center-kan: */
+.loader {
+  min-height: calc(1.5rem + 0.5rem); /* contoh supaya tinggi sama dengan button */
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
